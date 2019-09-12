5e1518ef4832d311049d9cbcf5befb9ae8d141fb
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2019-09-12 13:59:56 +0300

    update lex analysis method

--- src/LexicalAnalysis.java
+++ src/LexicalAnalysis.java
@@ -2,10 +2,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,88 +11,87 @@ import java.util.*;
 
 public class LexicalAnalysis {
 
-	private ArrayList<Token> delimiters;
-	private ArrayList<Token> identifiers;
-	private ArrayList<Token> literals;
-	private ArrayList<Token> keywords;
-	private ArrayList<Token> operators;
-
+    private ArrayList<Token> delimiters;
+    private ArrayList<Token> identifiers;
+    private ArrayList<Token> literals;
+    private ArrayList<Token> keywords;
+    private ArrayList<Token> operators;
     private ArrayList<Token> nonSupportedTokens;
 
-	private HashMap<Integer, String> delimitersList = new HashMap<>() {
-		{
-			put(1, "{");
-		}
-	};
-	private HashMap<Integer, String> operatorsList = new HashMap<>() {
-		{
-			put(1, "+");
-			put(2, "-");
-			put(3, "*");
-			put(4, "/");
-			put(5, ":=");
-			put(6, "div");
-			put(7, "mod");
-			put(8, "and");
-			put(9, "or");
-			put(10, "not");
-			put(11, "xor");
-			put(12, "<>"); //not equal
-			put(13, "<");
-			put(14, ">");
-			put(15, "=");
-			put(16, "<=");
-			put(17, "=<");
-			put(18, ">=");
-			put(20, "=>");
-			put(21, "in");
-		}
-	};
-	private HashMap<Integer, String> keywordsList = new HashMap<>() {
-		{
-			put(1, "begin");
-			put(2, "boolean");
-			put(3, "break");
-			put(4, "byte");
-			put(5, "do");
-			put(6, "double");
-			put(7, "if");
-			put(8, "else");
-			put(9, "end");
-			put(10, "false");
-			put(11, "true");
-			put(12, "integer");
-			put(13, "longint");
-			put(17, "shortint");
-			put(14, "repeat");
-			put(15, "shr");
-			put(16, "single");
-			put(18, "then");
-			put(19, "until");
-			put(20, "word");
-			put(21, "program");
-			put(22, "while");
-			put(23, "var");
-			put(24, "downto");
-			put(25, "label");
-			put(26, "record");
-			put(27, "with");
-			put(28, "procedure");
-			put(29, "goto");
-			put(30, "packed");
-			put(31, "const");
-			put(32, "type");
-			put(33, "case");
-			put(34, "function");
-			put(35, "to");
-			put(36, "of");
-			put(37, "for");
-			put(38, "array");
-			put(39, "file");
-			put(40, "set");
+    private HashMap<Integer, String> delimitersList = new HashMap<>() {
+        {
+            put(1, "{");
+        }
+    };
+    private HashMap<Integer, String> operatorsList = new HashMap<>() {
+        {
+            put(1, "+");
+            put(2, "-");
+            put(3, "*");
+            put(4, "/");
+            put(5, ":=");
+            put(6, "div");
+            put(7, "mod");
+            put(8, "and");
+            put(9, "or");
+            put(10, "not");
+            put(11, "xor");
+            put(12, "<>"); //not equal
+            put(13, "<");
+            put(14, ">");
+            put(15, "=");
+            put(16, "<=");
+            put(17, "=<");
+            put(18, ">=");
+            put(20, "=>");
+            put(21, "in");
+        }
+    };
+    private HashMap<Integer, String> keywordsList = new HashMap<>() {
+        {
+            put(1, "begin");
+            put(2, "boolean");
+            put(3, "break");
+            put(4, "byte");
+            put(5, "do");
+            put(6, "double");
+            put(7, "if");
+            put(8, "else");
+            put(9, "end");
+            put(10, "false");
+            put(11, "true");
+            put(12, "integer");
+            put(13, "longint");
+            put(17, "shortint");
+            put(14, "repeat");
+            put(15, "shr");
+            put(16, "single");
+            put(18, "then");
+            put(19, "until");
+            put(20, "word");
+            put(21, "program");
+            put(22, "while");
+            put(23, "var");
+            put(24, "downto");
+            put(25, "label");
+            put(26, "record");
+            put(27, "with");
+            put(28, "procedure");
+            put(29, "goto");
+            put(30, "packed");
+            put(31, "const");
+            put(32, "type");
+            put(33, "case");
+            put(34, "function");
+            put(35, "to");
+            put(36, "of");
+            put(37, "for");
+            put(38, "array");
+            put(39, "file");
+            put(40, "set");
 
-		}
-	};
+        }
+    };
 
     public HashMap<Integer, String> getDelimitersList() {
         return delimitersList;
@@ -118,14 +114,10 @@ public class LexicalAnalysis {
         operators = new ArrayList<>();
     }
 
-	@Nullable
-	@Contract(pure = true)
-	static ArrayList<Token> parser() {
-		return null;
-	}
 
-	public static void main(String[] args) throws IOException {
-		String sampleCode = "if (alpha>beta) then";
+    public static void main(String[] args) throws IOException {
+        //example  of tokenizator usage
+		/*String sampleCode = "if (alpha>beta) then";
 		InputStream fromString = new ByteArrayInputStream(sampleCode.getBytes());
 		InputStream fromFile = new FileInputStream("SamplePascal.pas");
 		Tokenizer tokenizer = new Tokenizer(fromFile);
@@ -142,62 +134,81 @@ public class LexicalAnalysis {
 			Token token = new Token(tok,1,1);
 			la.classify(token);
 		}
-		System.out.println("Delimiters tokens: \n");
-		for (Token t: la.delimiters){
-			System.out.println(t.name + " " + t.type + " " + t.line + " "+ t.place_at_line);
-		}
-		System.out.println();
-		System.out.println("Literals tokens: \n");
-		for (Token t: la.literals){
-			System.out.println(t.name + " " + t.type + " " + t.line + " "+ t.place_at_line);
-		}
-		System.out.println();
-		System.out.println("Operators tokens: \n");
-		for (Token t: la.operators){
-			System.out.println(t.name + " " + t.type + " " + t.line + " "+ t.place_at_line);
-		}
-		System.out.println();
-		System.out.println("Keywords tokens: \n");
-		for (Token t: la.keywords){
-			System.out.println(t.name + " " + t.type + " " + t.line + " "+ t.place_at_line);
-		}
-		System.out.println();
-		System.out.println("Identifiers tokens: \n");
-		for (Token t: la.identifiers){
-			System.out.println(t.name + " " + t.type + " " + t.line + " "+ t.place_at_line);
-		}
+		*/
+		LexicalAnalysis la = new LexicalAnalysis();
+		la.performLexicalAnalysis("SamplePascal.pas");
 
-		System.out.println("Lexical analysis DONE");
-
-	}
+    }
 
 
-	private void classify(@NotNull Token given) {
+    private void classify(@NotNull Token given) {
 
-		if (given.isDelimiter()) {
-			delimiters.add(given);
+        if (given.isDelimiter()) {
+            delimiters.add(given);
 
-		} else if (given.isOperator()) {
-			operators.add(given);
-		} else if (given.isKeyword()) {
-			keywords.add(given);
+        } else if (given.isOperator()) {
+            operators.add(given);
+        } else if (given.isKeyword()) {
+            keywords.add(given);
 
-		} else if (given.isLiteral()) {
-			literals.add(given);
+        } else if (given.isLiteral()) {
+            literals.add(given);
 
-		} else if (given.isIdentifier()) {
-			identifiers.add(given);
+        } else if (given.isIdentifier()) {
+            identifiers.add(given);
 
         } else {
             nonSupportedTokens.add(given);
         }
     }
 
-	public void performLexicalAnalysis(String path) {
-		//открыть файл
-		// спарсить токены
-		// по списку токенов пройти и проклассифицировать
-		// запринтить аутпут
-		// вы восхитительны
-	}
+    public void performLexicalAnalysis(String path) throws IOException {
+        //Open file
+        InputStream fromFile = new FileInputStream(path);
+        Tokenizer tokenizer = new Tokenizer(fromFile);
+
+        tokenizer.tokenize();
+        fromFile.close();
+
+        List<String> tokens = tokenizer.getTokens();
+
+        System.out.println("Lexical analysis START\n");
+        LexicalAnalysis la = new LexicalAnalysis();
+        for (String tok : tokens) {
+            Token token = new Token(tok, 1, 1);
+            la.classify(token);
+        }
+        System.out.println("Delimiters tokens: \n");
+        for (Token t : la.delimiters) {
+            System.out.println(t.name + " " + t.type + " " + t.line + " " + t.place_at_line);
+        }
+        System.out.println();
+        System.out.println("Literals tokens: \n");
+        for (Token t : la.literals) {
+            System.out.println(t.name + " " + t.type + " " + t.line + " " + t.place_at_line);
+        }
+        System.out.println();
+        System.out.println("Operators tokens: \n");
+        for (Token t : la.operators) {
+            System.out.println(t.name + " " + t.type + " " + t.line + " " + t.place_at_line);
+        }
+        System.out.println();
+        System.out.println("Keywords tokens: \n");
+        for (Token t : la.keywords) {
+            System.out.println(t.name + " " + t.type + " " + t.line + " " + t.place_at_line);
+        }
+        System.out.println();
+        System.out.println("Identifiers tokens: \n");
+        for (Token t : la.identifiers) {
+            System.out.println(t.name + " " + t.type + " " + t.line + " " + t.place_at_line);
+        }
+
+        System.out.println("Lexical analysis DONE");
+
+        //открыть файл
+        // спарсить токены
+        // по списку токенов пройти и проклассифицировать
+        // запринтить аутпут
+        // вы восхитительны
+    }
 }
