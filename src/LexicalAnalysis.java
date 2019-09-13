e103aa119601830b6d37d22ceb05fd79a25d01f0
elukyanchikova <31773897+elukyanchikova@users.noreply.github.com>
2019-09-13 12:04:05 +0300

    updated output to file

--- src/LexicalAnalysis.java
+++ src/LexicalAnalysis.java
@@ -103,6 +103,10 @@ public class LexicalAnalysis {
             put(39, "file");
             put(40, "set");
             put(41, "file");
+            put(42, "char");
+            put(43, "string");
+            put(44, "writeln");
+            put(45, "readln");
 
         }
     };
@@ -127,45 +131,6 @@ public class LexicalAnalysis {
         keywords = new ArrayList<>();
         operators = new ArrayList<>();
         nonSupportedTokens = new ArrayList<>();
-    }
-
-    @Nullable
-    @Contract(pure = true)
-    static ArrayList<Token> parser() {
-        return null;
-    }
-
-    public static void main(String[] args) throws IOException {
-        //example  of tokenizator usage
-		/*String sampleCode = "if (alpha>beta) then";
-		InputStream fromString = new ByteArrayInputStream(sampleCode.getBytes());
-		InputStream fromFile = new FileInputStream("SamplePascal.pas");
-		Tokenizer tokenizer = new Tokenizer(fromFile);
-
-		tokenizer.tokenize();
-		fromFile.close();
-
-		List<RawToken> tokens = tokenizer.getTokens();
-
-		System.out.println("Lexical analysis START\n");
-		LexicalAnalysis la = new LexicalAnalysis();
-		for (String tok : tokens) {
-			//System.out.println(tok);
-			Token token = new Token(tok,1,1);
-			la.classify(token);
-		for (RawToken tok : tokens) {
-			System.out.printf("[%d, %d] %s\n", tok.line, tok.place, tok.val);
-		}
-		*/
-        System.out.println("Input the name of .pas file (like 'Name.pas') or press Enter to watch a sample file: ");
-        Scanner in = new Scanner(System.in);
-        String inputFile = in.nextLine();
-        LexicalAnalysis la = new LexicalAnalysis();
-        if (inputFile.isEmpty()) {
-            inputFile = "SamplePascal.pas";
-        }
-        la.performLexicalAnalysis(inputFile, "Output.txt");
-
     }
 
 
